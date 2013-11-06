5795ce67acfdee904817650c8fe5879028f3244d
Alex Johnson <alexjohnson505@gmail.com>
2013-11-05 22:21:58 -0500

    Setup character json and basic modals

---
+++ data.js
@@ -0,0 +1,48 @@
+var characterData = {
+    "name": "Jpe SHMOE",
+    "class": "Fighter",
+    "race": "Half-Orc",
+    "hp": [
+        {
+            "time": "9:54 PM 11/5/2013",
+            "amount": 15,
+            "description": "Arrow to face"
+        },
+        {
+            "time": "9:55 PM 11/5/2013",
+            "amount": 10,
+            "description": "It was a fire Arrow"
+        }
+    ],
+    "ac": 21,
+    "str": 12,
+    "dex": 10,
+    "con": 8,
+    "wis": 12,
+    "cha": 16,
+    "int": 6,
+    "skills" : [
+        {
+            "name": "xxx",
+            "description": "Arrow to face",
+            "data": 15,
+        },
+        {
+            "name": "xxx",
+            "description": "Arrow to face again",
+            "data": 15,
+        },
+    ],
+    "attacks" : [
+        {
+            "name": "Shortsword",
+            "description": "Wide swipe",
+            "data": 15,
+        },
+        {
+            "name": "Fireball",
+            "description": "Fire attack",
+            "data": 15,
+        },
+    ],
+}
---
+++ functions.js
@@ -0,0 +1,12 @@
+// Load JSON text from server hosted file and return JSON parsed object
+function loadJSON() {
+  // Load json file;
+  var json = loadTextFileAjaxSync("character.json");
+  // Parse json
+  return JSON.parse(optionsText);
+} 
+function addModal(target) {
+	$('#add-modal .modal-title').html("TITLEYEA");
+	$('#add-modal .modal-body').html("yeah");
+	$('#add-modal').modal('show');
+}
