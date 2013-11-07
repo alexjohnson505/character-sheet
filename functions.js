373f032db7419c85aad2f17816e92a050a12f279
Alex Johnson <alexjohnson505@gmail.com>
2013-11-06 22:31:13 -0500

    Add weaponAttack rolling and templates for hit vs miss

--- functions.js
+++ functions.js
@@ -1,19 +1,37 @@
+
 // Return attribute modifier for given attribute
 function getAttributeMod (attribute){
 	return Math.floor((characterData[attribute] - 10)/2);
 }
+
+// Temporary fake-die-roll calculation
 function roll (die){
 	if (die == "1d20"){
 		return Math.floor(Math.random()*20);
 	} else if (die == "12d6"){
+		return Math.floor(Math.random()*72);
+	} else if (die == "1d6"){
 		return Math.floor(Math.random()*72);
 	} else {
 		return 0;
 	}
 }
 
+// Renders given data and open modal based on id
+function openModal (title, html) {
+	$('#roll-modal .modal-title').html(title);
+	$('#roll-modal .modal-body').html(html);
+	$('#roll-modal .modal-prompt').html();
+	$('#roll-modal .modal-footer').html(templates.footerClose);
+
+	  
+
+
+	$('#roll-modal').modal('show');
+}
+
 // Performs roll for a skill check
-function rollCheck (target) {
+function skillCheck (target) {
 	var skillName = target;
 	var skill;
 
@@ -28,24 +46,11 @@ function rollCheck (target) {
 	skillData['d20roll'] = roll("1d20");
 	skillData['sum'] = skillData['attributeMod'] + skillData['d20roll'];
 
-	var output = Mustache.render(templates.rollCheck, skillData);
-
-	openModal("#roll-check-modal", output);
+	var output = Mustache.render(templates.skillCheck, skillData);
+	openModal("Roll a Skill", output);
 }
 
-function rollAttack (target){
-	var attackName = target;
-	var attack;
-
-	for (var i = characterData.attacks.length - 1; i >= 0; i--) {
-		if (characterData.attacks[i].name == target) {
-			attack = characterData.attacks[i];
-		}
-	};
-
-	console.log(attack);
-}
-
+// Roll the damage for a spell
 function rollSpell (target){
 	var spellName = target;
 	var spell;
@@ -59,13 +64,43 @@ function rollSpell (target){
 	var spellData = spell;
 	spellData['damage'] = roll(spell.damage);
 	spellData['spellDC'] = 10 + getAttributeMod(spell.attribute);
+	spellData['attributeMod'] = getAttributeMod(spell.attribute);
 
 	var output = Mustache.render(templates.spellCast, spellData);
-	
-	openModal("#roll-spell-modal", output);
+	openModal("Cast a Spell", output);
 }
 
-function openModal (id, html) {
-	$(id + ' .modal-body').html(html);
-	$(id).modal('show');
+// Roll the to-hit and damage for an attack
+function rollAttack (target){
+	var attackName = target;
+	var attack;
+
+	for (var i = characterData.attacks.length - 1; i >= 0; i--) {
+		if (characterData.attacks[i].name == target) {
+			attack = characterData.attacks[i];
+		}
+	};
+
+	// global : attack data
+	// saves last used attacks information
+	attackData = attack;
+	attackData['attributeMod'] = getAttributeMod(attack.attribute);
+	attackData['d20roll'] = roll("1d20");
+	attackData['toHit'] = attackData['d20roll'] + attackData['attributeMod'];
+
+	var output = Mustache.render(templates.weaponAttack, attackData);
+	openModal("Make an Attack", output);
+	$('#roll-modal .modal-prompt').html(templates['hit-or-miss']);
+
+}
+
+function attackConfirmation (status){
+	
+	if (status == "hit"){
+		var damage = attackData['d20roll'] + attackData['attributeMod'] + roll(attackData['damage']);
+		$('#roll-modal .modal-prompt').html("<br>Your attack Hits!!!!<strong>" + damage + " DAMAGE</strong>");
+
+	} else if (status == "miss") {
+		$('#roll-modal .modal-prompt').html("Bummer. Your attack missed.   ");
+	}
 }
--- templates.js
+++ templates.js
@@ -1,7 +1,24 @@
 var templates = {
-	"spellCast" : "Name : {{name}}<br>Description : {{description}}<br>Damage Type : {{damage-type}}<br><strong>{{damage}} : DAMAGE</strong><br>Spell DC: {{spellDC}}",
-	"rollCheck" : "Skill: {{name}}<br>{{d20roll}} : 1d20 Base Roll<br>{{attributeMod}} : {{attribute}} Modifier<br><strong> {{sum}} : SUM</strong>"
+	"spellCast" : "Name : {{name}}<br>Description : {{description}}<br>Damage Type : {{damage-type}}<br>Attribute: {{attribute}}<br>Attribute Mod: {{attributeMod}}<br><br><strong>{{damage}} : DAMAGE</strong><br>Spell DC: {{spellDC}}",
+	"skillCheck" : "Skill: {{name}}<br>{{d20roll}} : 1d20 Base Roll<br>{{attributeMod}} : {{attribute}} Modifier<br><strong> {{sum}} : SUM</strong>",
+	"weaponAttack" : 'Name : {{name}}<br>Description : {{description}}<br>Damage : {{damage}}<br>Damage type : {{damage-type}}<br>Attribute: {{attribute}}<br>Attribute Mod: {{attributeMod}}<br>{{d20roll}} : 1d20 Roll<br>You roll a {{toHit}} To-Hit. Ask your DM - does this Hit or Miss?',
+	"hit-or-miss" : '<div class="hit-or-miss"><button type="button" class="btn btn-primary" onclick="attackConfirmation(\'hit\')";>Hit</button><button type="button" class="btn btn-danger" onclick="attackConfirmation(\'miss\')";>Miss</button></div>',
+	"footerClose" : '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
 }
 
 
+
+
+	
+
+// Weapon:
+// Damage:
+// Ability Mod:
+
+// 1d20 Roll
+// Ability Mod:
+
+// Hit vs Miss
+
+// Damage
 
