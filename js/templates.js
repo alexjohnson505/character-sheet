3bf85885c53da7e02a3125e5837026491eadde69
Alex Johnson <alex.johnson@evertrue.com>
2013-11-28 13:34:36 -0500

    Character data not in input forms. Added save button

--- js/templates.js
+++ js/templates.js
@@ -1,5 +1,5 @@
 var templates = {
-    "viewSpell" : 
+    "viewSpell" :
         '<table class="table table-hover"><tr><td>Name</td><td>{{name}}</td></tr>\
         <tr><td>Description</td><td>{{description}}</td></tr>\
         <tr><td>Damage Typ</td><td>{{damage-type}}</td></tr>\
@@ -7,13 +7,13 @@ var templates = {
         <tr><td>Attribute Mod</td><td>{{attributeMod}}</td></tr>\
         <tr><td>Spell DC</td><td>{{spellDC}}</td></tr></table>',
     "rollSpell" : "Your spell {{name}} inflicts {{inflictedDamage}}. <br> Calculated by rolling {{damage}}",
-    
-    "viewSkill" : 
+
+    "viewSkill" :
         '<table class="table table-hover"><tr><td>Skill Name</td><td>{{name}}</td></tr>\
         <tr><td>{{attributeMod}}</td><td>{{attribute}} Modifier</td></tr></table>',
     "rollMods" : '<strong>{{title}}</strong>: <table class="roll-table">{{#rollcomps}}<tr><td><strong>{{spec}}</strong></td><td>{{{number}}}</td><td>{{{desc}}}</td></tr>{{/rollcomps}}<tr><td></td><td>{{sum}}</td><td><strong>total</strong></td></tr></table>',
     "dmPrompt" : '<p class="triangle-isosceles top">{{message}}</p>',
-    "viewAttack" : 
+    "viewAttack" :
         '<table class="table table-hover"><tr><td>Name</td><td>{{name}}</td></tr>\
         <tr><td>Description</td><td>{{description}}</td></tr>\
         <tr><td>Damage</td><td>{{damage}}</td></tr>\
@@ -24,13 +24,13 @@ var templates = {
     "rollAttack" : "Your attack Hits!!! You inflict {{inflictedDamage}} Damage. <br> Calculated by rolling {{damageRoll}} ({{damage}} die) + {{attributeMod}} Ability Mod",
 
     "edit-or-roll" : '<div class="hit-or-miss"><button type="button" class="btn btn-default" onclick="edit{{type}}()";>Edit</button><button type="button" class="btn btn-danger" onclick="roll{{type}}()";>Roll</button></div>',
-    
+
     "footerClose" : '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
     "abilityScore" : "<tr><td>{{attributeName}}</td><td>{{attribute}}</td><td>{{attributeMod}}</td></tr>",
-    "characterStats" : '<div class="img-thumbnail"><i class="fa fa-picture-o"></i></div><form class="form-horizontal"><div class="form-group"><label class="col-sm-4 control-label">Name</label><div class="col-sm-8"><p type="email" class="form-control">{{name}}</p></div><label class="col-sm-4 control-label">Class</label><div class="col-sm-8"><p type="email" class="form-control">{{class}}</p></div><label class="col-sm-4 control-label">Race</label><div class="col-sm-8"><p type="email" class="form-control">{{race}}</p></div><label class="col-sm-4 control-label"><i class="fa fa-heart"></i> HP</label><div class="col-sm-8"><p type="email" class="form-control">{{currentHP}}</p></div><label class="col-sm-4 control-label"><i class="fa fa-shield"></i>AC</label><div class="col-sm-8"><p type="email" class="form-control">{{currentAC}}</p></div></div></form>',
+    "characterStats" : '<div class="img-thumbnail"><i class="fa fa-picture-o"></i></div><form class="form-horizontal"><div class="form-group"><label class="col-sm-4 control-label">Name</label><div class="col-sm-8"><input type="name" class="form-control" value="{{name}}"></input></div><label class="col-sm-4 control-label">Class</label><div class="col-sm-8"><input type="class" class="form-control" value="{{class}}""></input></div><label class="col-sm-4 control-label" >Race</label><div class="col-sm-8"><input type="email" class="form-control" value="{{race}}"></input></div><label class="col-sm-4 control-label"><i class="fa fa-heart"></i> HP</label><div class="col-sm-8"><input type="currentHP" class="form-control" value="{{currentHP}}"></input></div><label class="col-sm-4 control-label"><i class="fa fa-shield"></i>AC</label><div class="col-sm-8"><input type="currentAC" class="form-control" value="{{currentAC}}"></input></div></div></form><button class="btn btn-default">Save Changes</button>',
 
     "die-animation" : "<div class='die-animation'></div>",
-    
+
     "skill"  :  '<tr><td><i class="fa {{icon}}"></td><td>{{name}}</td><td class="die" onclick="viewSkill(\'{{name}}\');"></td></tr>',
     "attack" :  '<tr><td><i class="fa {{icon}}"></td><td>{{name}}</td><td class="die" onclick="viewAttack(\'{{name}}\');"></td></tr>',
     "spell"  :  '<tr><td><i class="fa {{icon}}"></td><td>{{name}}</td><td class="die" onclick="viewSpell(\'{{name}}\');"></td></tr>',
